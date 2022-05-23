ba649ba63005a181d97dbfecdc96d70540a593fe
Vasiliy Sazonov <f7m239d02k@gmail.com>
2022-05-23 22:15:38 +0300

    transform fix

--- engine/logic/base/base.hpp
+++ engine/logic/base/base.hpp
@@ -47,9 +47,9 @@ namespace Logical
 
 			model = glm::translate(model, position);
 
-			model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1, 0, 0));
-			model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0, 1, 0));
 			model = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0, 0, 1));
+			model = glm::rotate(model, glm::radians(rotation.y), glm::vec3(0, 1, 0));
+			model = glm::rotate(model, glm::radians(rotation.x), glm::vec3(1, 0, 0));
 
 			model = glm::scale(model, scale);
 		}
