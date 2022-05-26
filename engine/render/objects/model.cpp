0cca42999f0f0a04ee62b6b6cdec2a611b1e78d4
Vasiliy Sazonov <f7m239d02k@gmail.com>
2022-05-26 06:06:48 +0300

    repetitive texture rendering fix

--- engine/render/objects/model.cpp
+++ engine/render/objects/model.cpp
@@ -113,8 +113,8 @@ std::vector<Texture> Model::getMaterialTextures(aiMaterial *mat, aiTextureType t
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
 
